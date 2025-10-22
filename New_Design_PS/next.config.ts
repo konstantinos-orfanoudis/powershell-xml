bde83164bd2e6074f7f4f40e5d4be59c98d18102
konstantinos-orfanoudis <konstantinos.orfanoudis@intragen.com>
2025-10-22 11:01:17 +0000

    deployment changes

--- New_Design_PS/next.config.ts
+++ New_Design_PS/next.config.ts
@@ -1,5 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true }, // don't fail builds on lint
+// New_Design_PS/next.config.ts
+import path from "path";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname),
+  productionBrowserSourceMaps: false,
+  eslint: { ignoreDuringBuilds: true },
+  // output: 'standalone',
 };
+
 export default nextConfig;
